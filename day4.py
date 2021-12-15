26a56905f151054ad5de8f15c31d17d39fd98755
Ilia Ilmer <i.ilmer@icloud.com>
2021-12-14 22:18:40 -0500

    add some type hints and small error fix

--- day4.py
+++ day4.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 
 test_input = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
@@ -23,7 +23,7 @@ test_input = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,
 
 
 class Board:
-    def __init__(self, rows: list) -> None:
+    def __init__(self, rows: List[str]) -> None:
         self.rows = [list(map(int, r.split())) for r in rows]
         self.mask = [[0 for _ in r] for r in self.rows]
 
@@ -36,7 +36,7 @@ class Board:
         )
 
 
-def get_winning_board(input: str):
+def get_winning_board(input: str) -> Tuple[List[Board], int]:
     input = input.split("\n\n")
     numbers, boards = list(map(int, input[0].split(","))), input[1:]
     boards = [Board(b.split("\n")) for b in boards]
@@ -48,7 +48,7 @@ def get_winning_board(input: str):
                     break
             row_wise = [a == b for (a, b) in zip(board.mask, board.rows)]
             col_wise = [
-                board.mask[:][c] == board.rows[:][c]
+                [m[c] for m in board.mask] == [r[c] for r in board.rows]
                 for c in range(len(board.rows[0]))
             ]
             if any(row_wise) or any(col_wise):
@@ -56,7 +56,7 @@ def get_winning_board(input: str):
     return boards, num
 
 
-def get_last_winning_board(input: str):
+def get_last_winning_board(input: str) -> Tuple[List[Board], int]:
     input = input.split("\n\n")
     numbers, boards = list(map(int, input[0].split(","))), input[1:]
     boards = [Board(b.split("\n")) for b in boards]
